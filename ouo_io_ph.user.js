759f79a2de0739633d197e259881074eaa012ca4
rjmolina13 <38134522+rjmolina13@users.noreply.github.com>
2018-04-06 22:18:47 +0800

    Updated metadate

--- ouo_io_ph.user.js
+++ ouo_io_ph.user.js
@@ -2,28 +2,25 @@
 // @name        ouo.io block-bypass 4 ph
 // @namespace   http://ronanj.co.nr
 // @description Redirect ouo.io to ph-safe ouo
-// @include     http://ouo.io/*
-// @include     http://*.ouo.io/*
-// @include     https://*.ouo.io/*
-// @include     https://ouo.io/*
+// @author	Ronanj T. Molina <rj.molina13@gmail.com>
+// @match    *://ouo.io/*
+// @run-at   document-start
+// @grant    none
 // @version     0.1
-// @grant       none
+// @icon	http://ouo.press/blog/favicon.png
+// @license         GPLv3 - http://www.gnu.org/licenses/gpl-3.0.txt
 // ==/UserScript==
  
 
-
-var url = window.location.host;
- 
-
-if (url.match("//ouo.io") === null) {
-    url = window.location.href;
-    if  (url.match("//ouo.io") !== null){
-        url = url.replace("//ouo.io", "//ouo.press");
-    } else if (url.match("/*.html") !== null){
-        url = url.replace("/*.html", " ");
-    } else {
-    	return;
-    }
-    console.log(url);
-    window.location.replace(url);
+//-- Only redirect if the *path* ends in .html...
+if (/\.html$/.test (location.pathname) ) {
+    var newHost     = location.host.replace (/\.io$/, ".press");
+    var plainPath   = location.pathname.replace (/\.html$/, "");
+    var newURL      = location.protocol + "//" +
+        newHost                  +
+        plainPath                +
+        location.search          +
+        location.hash
+    ;
+    location.replace (newURL);
 }
